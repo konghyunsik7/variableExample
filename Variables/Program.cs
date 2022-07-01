e6d12713e12497638be4a9db0d354a41f4a1313c
Hyun Kong <konghyunsik7@gmail.com>
2022-07-01 12:42:09 -0400

    initial exmaple

---
+++ Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿string myName = "Hyun Sik Kong aka Stan";
+
+int myAge = 100;
+
+char letter = '!';
+
+bool twoCats = true;
+
+double catDad = 1.5;
+
+decimal costofCatfood = 99.99m;
+
+
+Console.WriteLine($"My names is {myName} and I feel like {myAge} years old due to my back pain{letter} It's {twoCats} that I am a catdad {catDad}.I spend weekly of ${costofCatfood} on cat food.");
+
+
+
+
